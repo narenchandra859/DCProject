99670d7f4f435b6da3fb19f3a3e21b5b530dc221
Naren Chandra <44522774+narenchandra859@users.noreply.github.com>
2019-04-30 09:40:11 +0530

    Update goBackN.py

--- goBackN.py
+++ goBackN.py
@@ -8,15 +8,15 @@ def getPacket(packNo, senderPack, receiverPack):
     global NumberOfPackets, PacketGenTime, TransmissionTime, TimeoutTime, PackLossPerc, totTime
     if(packNo<=NumberOfPackets):
         t.sleep(PacketGenTime/1000)
-        print("\nPacket ",packNo,"is generated at time : ",totTime)
+        print("\nS:\tPacket ",packNo,"is generated at time : ",totTime)
         (checkList[senderPack][0],checkList[senderPack][1])=(packNo,totTime)
         (checkList[senderPack][2],checkList[senderPack][3]) = (0,0)
         c=r.randint(1,100)
         if(c<=PackLossPerc):
-            print("Packet will be lost/Won't be transmitted\n")
+            print("S:\tPacket will be lost/Won't be transmitted\n")
             checkList[senderPack][3]=totTime+TimeoutTime
         else:
-            print("Packet will be transmitted without loss\n")
+            print("S:\tPacket will be transmitted without loss\n")
             checkList[senderPack][2]=totTime + (2*TransmissionTime)
 
 def GoBackN():
@@ -27,12 +27,13 @@ def GoBackN():
     while(1):
         TimedOut=False
         if(checkList[receiverPacket][2]<=totTime+PacketGenTime and checkList[receiverPacket][2]!=0):
-            print("\tPacket ",checkList[receiverPacket][0],"has been acknowledged. Time = ",checkList[receiverPacket][2])
+            print("R:\tPacket ",checkList[receiverPacket][0],"has been acknowledged. Time = ",checkList[receiverPacket][2])
             if(checkList[receiverPacket][0]>=NumberOfPackets):
                 done()
             receiverPacket=receiverPacket+1
         elif(checkList[receiverPacket][3]<=totTime+PacketGenTime and checkList[receiverPacket][3]!=0):
-            print("\tTimed out! Packet",checkList[receiverPacket][0],"hasn't been acknowledged. Time = ",checkList[receiverPacket][3])
+            t.sleep((checkList[receiverPacket][3]-totTime)/1000)
+            print("R:\tTimed out! Packet",checkList[receiverPacket][0],"hasn't been acknowledged. Time = ",checkList[receiverPacket][3])
             sendPacket=receiverPacket
             TimedOut=True
             packNo=checkList[receiverPacket][0]
@@ -47,7 +48,7 @@ def GoBackN():
         packNo=packNo+1
         sendPacket=sendPacket+1
 
-print("Enter the parameters (in ms) : \n\n")
+print("Enter the parameters (in ms)")
 NumberOfPackets=int(input("Enter the number of packets to send : "))
 PacketGenTime=int(input("Enter the time taken to generate a packet : "))
 TransmissionTime=int(input("Enter the transmission time (assume it to be same from S->R and R->S) : "))
