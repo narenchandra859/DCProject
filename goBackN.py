f0d70dfc9b28893a766dc58d3c74e9e6296fe0bd
narenchandra859 <narenchandra859@gmail.com>
2019-04-30 09:21:42 +0530

    stablev2

--- goBackN.py
+++ goBackN.py
@@ -1,12 +1,14 @@
 import random as r
 import sys
+import time as t
 def done():
-    print("Protocol has been executed.")
+    print("----------------------Protocol has been executed.------------------------")
     sys.exit(0)
 def getPacket(packNo, senderPack, receiverPack):
     global NumberOfPackets, PacketGenTime, TransmissionTime, TimeoutTime, PackLossPerc, totTime
     if(packNo<=NumberOfPackets):
-        print("Packet ",packNo,"is generated at time : ",totTime)
+        t.sleep(PacketGenTime/1000)
+        print("\nPacket ",packNo,"is generated at time : ",totTime)
         (checkList[senderPack][0],checkList[senderPack][1])=(packNo,totTime)
         (checkList[senderPack][2],checkList[senderPack][3]) = (0,0)
         c=r.randint(1,100)
@@ -25,12 +27,12 @@ def GoBackN():
     while(1):
         TimedOut=False
         if(checkList[receiverPacket][2]<=totTime+PacketGenTime and checkList[receiverPacket][2]!=0):
-            print("\n\tPacket ",checkList[receiverPacket][0],"has been acknowledged. Time = ",checkList[receiverPacket][2])
+            print("\tPacket ",checkList[receiverPacket][0],"has been acknowledged. Time = ",checkList[receiverPacket][2])
             if(checkList[receiverPacket][0]>=NumberOfPackets):
                 done()
             receiverPacket=receiverPacket+1
         elif(checkList[receiverPacket][3]<=totTime+PacketGenTime and checkList[receiverPacket][3]!=0):
-            print("\n\tTimed out! Packet",checkList[receiverPacket][0],"hasn't been acknowledged. Time = ",checkList[receiverPacket][3])
+            print("\tTimed out! Packet",checkList[receiverPacket][0],"hasn't been acknowledged. Time = ",checkList[receiverPacket][3])
             sendPacket=receiverPacket
             TimedOut=True
             packNo=checkList[receiverPacket][0]
@@ -45,7 +47,7 @@ def GoBackN():
         packNo=packNo+1
         sendPacket=sendPacket+1
 
-print("Enter the parameters : \n\n")
+print("Enter the parameters (in ms) : \n\n")
 NumberOfPackets=int(input("Enter the number of packets to send : "))
 PacketGenTime=int(input("Enter the time taken to generate a packet : "))
 TransmissionTime=int(input("Enter the transmission time (assume it to be same from S->R and R->S) : "))
